d15e1e592f1b66f275a6f4fa59d1c20eaf03b6b6
Marco A. Ramírez Hdez. <marco.ramirez@utxicotepec.edu.mx>
2024-09-25 14:34:04 -0600

    Practica 09
    Agregación del Código del Ejecicio 07 de repaso de Clases

---
+++ DartPad/Ex07_Classes.dart
@@ -0,0 +1,43 @@
++void main() {
+  // Clase con parámetros posicionales los parámetros deben respetar su orden para ser asignados en la propoiedad
+  final wolverine = Hero('Logan', 'Regeneración');
+  // Clase con parámetros no posicionales, los puedo mandar en desorden
+  final xmen = Team(type: 'Heroes', name: 'X-Men');
+  final brotherhood = Team(name: "The Brotherhood of Evil Mutants");
+
+  
+  print(wolverine.name);
+  print(wolverine.power);
+  print("----------------------------------");
+  print(xmen);
+  print(xmen.name);
+  print(xmen.type);
+  // Invocando al método reescrito con @override
+  print(xmen.toString());
+  print("----------------------------------");
+    print(brotherhood.name);
+  print(brotherhood.type);
+  // Invocando al método reescrito con @override
+  print(brotherhood.toString());
+}
+
+class Hero {
+  String name;
+  String power;
+
+  Hero(String pName, String pPower)
+      : name = pName,
+        power = pPower;
+}
+
+class Team {
+  String name;
+  String type;
+
+  Team({required this.name, this.type = "No definido"});
+
+  @override
+  String toString() {
+    return 'Grupo: $name, Tipo: $type';
+  }
+}
